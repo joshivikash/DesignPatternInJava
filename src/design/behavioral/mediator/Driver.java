2c392f2a1d8ccc940ade5d79eaf9b563423e8a4f
Vikash Joshi <joshi.vikash17@gmail.com>
2018-04-09 15:07:34 +0530

    Giving some meaningful user names

--- src/design/behavioral/mediator/Driver.java
+++ src/design/behavioral/mediator/Driver.java
@@ -4,9 +4,9 @@ public class Driver {
     public static void main(String[] args) {
         ChatServer chatServer = new ChatServerImpl();
 
-        User u1 = new User("vicky", chatServer);
-        User u2 = new User("nicky", chatServer);
-        User u3 = new User("gudia", chatServer);
+        User u1 = new User("user1", chatServer);
+        User u2 = new User("user2", chatServer);
+        User u3 = new User("user3", chatServer);
 
         u1.sendMessage("Hi All !!!");
 
